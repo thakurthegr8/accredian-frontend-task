1f5928469d380a63439c07bb04ccfae41eac936b
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-12-09 16:55:29 +0530

    server url fixes

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { SERVER_URL } from "../../constants";
 
-const AUTH_URL = "https://thakurthegr8-accredian-server.onrender.com/auth";
+const AUTH_URL = `${SERVER_URL}/auth`;
 
 const AuthService = axios.create({ baseURL: AUTH_URL });
 
--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { SERVER_URL } from "../../constants";
 
-const USER_URL = "https://thakurthegr8-accredian-server.onrender.com/users";
+const USER_URL = `${SERVER_URL}/users`;
 
 const UserService = axios.create({ baseURL: USER_URL });
 
